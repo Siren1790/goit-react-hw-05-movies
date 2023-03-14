3fff74a2046bfc081143380cd6a0b62300fab59e
Sergii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-03-15 01:35:08 +0200

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,16 @@ import { useEffect, useState } from 'react';
 import { getTrandingMovies } from '../../ApiService/apiService.js';
 
 export const Home = () => {
-  const [page, setPage] = useState(3);
-  const [totalPages, setTotalPages] = useState(null);
+  // const [page, setPage] = useState(3);
+  // const [totalPages, setTotalPages] = useState(null);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    getTrandingMovies(page).then(({ results, total_pages, page }) => {
-      setTotalPages(total_pages);
+    getTrandingMovies(1).then(({ results, total_pages, page }) => {
+      // setTotalPages(total_pages);
       setMovies([...results]);
     });
-  }, [page]);
+  }, []);
 
   return (
     <main>
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -14,7 +14,7 @@ export const MovieDetails = () => {
       });
     };
     app();
-  },[]);
+  },);
 
   return (
     <main>
